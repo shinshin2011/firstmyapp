e72c5a00369324a1e7fecb9365bedda9699c9b27
shinshin2011 <94036169+shinshin2011@users.noreply.github.com>
2021-11-10 14:28:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import speech_recognition as sr
 
 app = Flask(__name__)
 
@@ -7,7 +8,28 @@ def index():
     return render_template("index.html")
 @app.route('/action')
 def act():
-    print("Hello")
-    
+    global text_jp
+    text1 = 'Hello'
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("話しして下さい。")
+            audio = r.listen(source)
+            
+        try:
+            # Google Web Speech APIで音声認識
+            text_jp = r.recognize_google(audio, language="ja-JP")
+        except sr.UnknownValueError:
+            print("Google Web Speech APIは音声を認識できませんでした。")
+        except sr.RequestError as e:
+            print("GoogleWeb Speech APIに音声認識を要求できませんでした;"
+                  " {0}".format(e))
+        else:
+            print(text_jp)
+
+        time.sleep(0.5)
+        break
+    print("完了。")
+    return render_template("index.html",myresult = text_jp)
 if (__name__) == "__main__":
     app.run(host='0.0.0.0')
