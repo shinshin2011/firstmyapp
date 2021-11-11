8bcae6e59d356e939b3c3eb45e28448a7727b8f0
shinshin2011 <94036169+shinshin2011@users.noreply.github.com>
2021-11-11 08:58:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
-import speech_recognition as sr
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    text2 = "สวัสดี สบายดีไหม"
-    translator = googletrans.Translator()
-    text_result = translator.translate(text2, src='auto', dest='ja')
+    
     return render_template("speech.html", myresult = text_result.text))
 @app.route('/action')
 def act():
