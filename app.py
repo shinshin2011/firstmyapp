ffae182d5dfc9e58f633f1bd73c11fccec3ecdaa
shinshin2011 <94036169+shinshin2011@users.noreply.github.com>
2021-11-17 12:01:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
-
-
+import requests
+from pprint import pprint
 app = Flask(__name__)
-
+API_HOST = 'https://api.bitkub.com'
 @app.route('/')
 def index():
-    
-    return render_template("home.html")
-@app.route('/action')
-def act():
-    
-    return render_template("index.html",myresult = "Testing")
+    response = requests.get(API_HOST + '/api/market/ticker')
+    result = response.json()
+
+    sym = 'THB_XRP'
+    data = result[sym]
+    last = data['last']
+    #print(data)
+    print(last)
+    return render_template("myapp.html", mydata=last)
+
+
 if (__name__) == "__main__":
     app.run(host='0.0.0.0')
